8abd287296794efb8ab1b4f33845bbd93c2b35f9
Yulia Tarima <yuliatarima@gmail.com>
2024-05-12 18:19:50 -0400

    Celebrate the first program that is calculating the area and perimeter of the rectangle

--- src/YuliaTarima_01_09.java
+++ src/YuliaTarima_01_09.java
@@ -4,36 +4,30 @@
 // This program displays the area and perimeter
 // of a rectangle with a width of 4.5 and a height of 7.9.
 
-// First the program defines a Rectangle class
-// with a main method where the calculations are done.
-// Then it declares variables for width and height,
-// and calculates the area and perimeter using the following formulas:
+// This Java program begins execution in the main method.
+// It declares variables for width and height,
+// then it calculates the area and perimeter using the following formulas:
 // area = width x height
 // perimeter = 2 x (width + height).
-// The results are then displayed to the user.
+// Finally, the results are displayed to the user.
 
 public class YuliaTarima_01_09 {
+    // Main method where program execution begins
+    public static void main(String[] args) {
 
-    // Define the Rectangle class
-    public class Rectangle {
+        // Declare variables for width and height of the rectangle
 
-        // Main method where program execution begins
-        public static void main(String[] args) {
+        double width = 4.5; // Width of the rectangle of double datatype
+        double height = 7.9; // Height of the rectangle
 
-            // Declare variables for width and height of the rectangle
+        // Calculate the area of the rectangle
+        double area = width * height; // Area = width x height
 
-            double width = 4.5; // Width of the rectangle of double datatype
-            double height = 7.9; // Height of the rectangle
+        // Calculate the perimeter of the rectangle
+        double perimeter = 2 * (width + height); // Perimeter = 2 x (width + height)
 
-            // Calculate the area of the rectangle
-            double area = width * height; // Area = width x height
-
-            // Calculate the perimeter of the rectangle
-            double perimeter = 2 * (width + height); // Perimeter = 2 x (width + height)
-
-            // Display the calculated area and perimeter
-            System.out.println("Area of the rectangle: " + area);
-            System.out.println("Perimeter of the rectangle: " + perimeter);
-        }
+        // Display the calculated area and perimeter
+        System.out.println("Area of the rectangle is: " + area);
+        System.out.println("Perimeter of the rectangle is: " + perimeter);
     }
 }
