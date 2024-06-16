f4460ac80095f61275d616bebe376e97792cccfc
Yulia Tarima <yuliatarima@gmail.com>
2024-06-15 21:41:21 -0400

    create 2 rectangles based on rectangle class with properties of height, width, perimeter, and area

---
+++ src/YuliaTarima_09_1.java
@@ -0,0 +1,55 @@
+// Yulia Tarima
+// Chapter 9 Assignment 1
+
+// This program defines a class named Rectangle_YT
+// to represent a rectangle with properties width and height.
+// It includes methods to calculate the area and perimeter of the rectangle.
+// A test program creates two specific rectangles
+// and prints their width, height, area, and perimeter.
+
+class Rectangle_YT {
+    // Data fields with default values
+    double width = 1;
+    double height = 1;
+
+    // No-arg constructor that creates a default rectangle
+    Rectangle_YT() {}
+
+    // Constructor that creates a rectangle with specified width and height
+    Rectangle_YT(double newWidth, double newHeight) {
+        width = newWidth;
+        height = newHeight;
+    }
+
+    // Method to get the area of the rectangle
+    double getArea() {
+        return width * height;
+    }
+
+    // Method to get the perimeter of the rectangle
+    double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+public class YuliaTarima_09_1 {
+    public static void main(String[] args) {
+        // Create two Rectangle objects
+        Rectangle_YT rectangle1 = new Rectangle_YT(4, 40);
+        Rectangle_YT rectangle2 = new Rectangle_YT(3.5, 35.9);
+
+        // Display the properties of the first rectangle
+        System.out.println("Rectangle 1:");
+        System.out.println("Width: " + rectangle1.width);
+        System.out.println("Height: " + rectangle1.height);
+        System.out.println("Area: " + rectangle1.getArea());
+        System.out.println("Perimeter: " + rectangle1.getPerimeter());
+
+        // Display the properties of the second rectangle
+        System.out.println("\nRectangle 2:");
+        System.out.println("Width: " + rectangle2.width);
+        System.out.println("Height: " + rectangle2.height);
+        System.out.println("Area: " + rectangle2.getArea());
+        System.out.println("Perimeter: " + rectangle2.getPerimeter());
+    }
+}
